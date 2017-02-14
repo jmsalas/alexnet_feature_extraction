38da94aaa7028153a8cea3009db44f01c6007369
Juan Salas <juan.salas@celerative.com>
2017-02-14 08:31:51 -0300

    Added code to download data files if not present.

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -5,6 +5,34 @@ from sklearn.model_selection import train_test_split
 from alexnet import AlexNet
 from sklearn.utils import shuffle
 
+from urllib.request import urlretrieve
+from os.path import isfile
+from tqdm import tqdm
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+if not isfile('train.p'):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='Train Dataset') as pbar:
+        urlretrieve(
+            'https://s3.amazonaws.com/udacity-sdc/datasets/german_traffic_sign_benchmark/train.p',
+            'train.p',
+            pbar.hook)
+
+if not isfile('test.p'):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='Test Dataset') as pbar:
+        urlretrieve(
+            'https://s3.amazonaws.com/udacity-sdc/datasets/german_traffic_sign_benchmark/test.p',
+            'test.p',
+            pbar.hook)
+
+print('Training and Test data downloaded.')
+
 # TODO: Load traffic signs data.
 sign_names = pd.read_csv('signnames.csv')
 n_classes = 43
@@ -57,7 +85,7 @@ probs = tf.nn.softmax(logits)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation)
+operation = optimizer.minimize(loss_operation)
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
